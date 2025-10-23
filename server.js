bf81177c36f9045b8a86003910c61e05c728b81c
qwe0972023450-cloud <qwe0972023450@gmail.com>
2025-10-23 20:19:59 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
-const express=require('express');const path=require('path');const app=express();const PORT=process.env.PORT||3000;app.use(express.static(__dirname));app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'index.html')));app.listen(PORT,()=>console.log('BAM 1.6.3 on '+PORT));
+const express=require('express');const path=require('path');const app=express();const PORT=process.env.PORT||8080;
+app.use(express.static(__dirname,{extensions:['html']}));
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'index.html')));
+app.listen(PORT,()=>console.log('BAM v1.6.4 on',PORT));
